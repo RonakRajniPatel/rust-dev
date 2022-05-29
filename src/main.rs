84eaf3373aca9c30de04b83feb99054637497435
RonakRajniPatel <RonakRajniPatel@gmail.com>
2022-05-29 17:02:08 -0400

    Guessing Game

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("Hello, world!");
     println!("The rust println function uses a !");
     println!("Guess a number and input your guess!");
-    let mut guess = String::new();
+    let mut guess = String::new(); // mutatable
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
