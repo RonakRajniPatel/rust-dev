c6986ffdcf6a5c3b6e2b14705851effc0caeb54e
RonakRajniPatel <RonakRajniPatel@gmail.com>
2022-05-29 17:45:01 -0400

    secret number via rng

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
-    println!("The rust println function uses a !");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is {}", secret_number);
     println!("Guess a number and input your guess!");
     let mut guess = String::new(); // mutatable
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
     println!("You guessed: {}", guess);
 
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
